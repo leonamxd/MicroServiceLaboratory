c8b20df14e30253b8ddaa702fb6c9cff097d7c3d
Leonam Sousa <59536649+leonamxd@users.noreply.github.com>
2023-05-06 23:53:56 -0300

    Start Project

---
+++ src/main/java/br/ucsal/MicroServiceLaboratoryApplication.java
@@ -0,0 +1,13 @@
+package br.ucsal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MicroServiceLaboratoryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MicroServiceLaboratoryApplication.class, args);
+	}
+
+}
